99ac3ff03999686387ae3faf3891cb0087e2f5c5
Kilian Pinguet <114613491+Kilian7213@users.noreply.github.com>
2024-12-06 21:52:47 +0100

    initial commit starting up project

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { AllItemsComponent } from './pages/all-items/all-items.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: AllItemsComponent,
+  },
+];
