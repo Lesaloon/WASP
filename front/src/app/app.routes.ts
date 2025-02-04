dc2d919a3fdea2d8b9b84af34743fb05ba833749
Lesaloon <47109666+Lesaloon@users.noreply.github.com>
2025-02-04 14:33:24 +0100

    Add AuthGuard to protect the home route and implement route guarding

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -1,11 +1,13 @@
 import { Routes } from '@angular/router';
 import { AccueilComponent } from './pages/accueil/accueil.component';
 import { LoginComponent } from './pages/login/login.component';
+import { AuthGuard } from './guard/auth-guard.guard';
 
 export const routes: Routes = [
 	{
 		path: '',
 		component: AccueilComponent,
+		canActivate: [AuthGuard],
 	},
 	{
 		path: 'login',
